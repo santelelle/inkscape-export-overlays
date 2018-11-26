526b7df8df98bb995843d7357a2d70f2384437a6
Hendrik Fichtenberger <oss@hendrik-fichtenberger.de>
2018-11-26 15:59:29 +0100

    Add layer set feature (similar to view feature of Ipe)

--- export_layers.py
+++ export_layers.py
@@ -8,6 +8,7 @@ import subprocess
 import tempfile
 import shutil
 import copy
+import re
 
 
 class PNGExport(inkex.Effect):
@@ -15,39 +16,51 @@ class PNGExport(inkex.Effect):
         """init the effetc library and get options from gui"""
         inkex.Effect.__init__(self)
         self.OptionParser.add_option("--path", action="store", type="string", dest="path", default="~/", help="")
+        self.OptionParser.add_option("--filename_prefix", action="store", type="string", dest="filename_prefix", default="", help="")
         self.OptionParser.add_option('-f', '--filetype', action='store', type='string', dest='filetype', default='jpeg', help='Exported file type')
         self.OptionParser.add_option("--crop", action="store", type="inkbool", dest="crop", default=False)
         self.OptionParser.add_option("--dpi", action="store", type="float", dest="dpi", default=90.0)
 
+    def is_layer_in_range(self, layer, current):
+        if isinstance(layer[2], tuple) and layer[2][0] == 'numbered':
+            return layer[2][1] <= current and current <= layer[2][2]
+        else:
+            return false
+
     def effect(self):
         output_path = os.path.expanduser(self.options.path)
         curfile = self.args[-1]
-        layers = self.get_layers(curfile)
-        counter = 1
+        layers, maxlayer = self.get_layers(curfile)
 
-        for (layer_id, layer_label, layer_type) in layers:
-            if layer_type == "fixed":
-                continue
+        for counter in range(1, maxlayer+1):
+            for (layer_id, layer_label, layer_type) in layers:
+                if layer_type == "fixed":
+                    continue
 
-            show_layer_ids = [layer[0] for layer in layers if layer[2] == "fixed" or layer[0] == layer_id]
+                show_layer_ids = [layer[0] for layer in layers if layer[2] == "fixed" or (layer[2] == "export" and layer[0] == layer_id) or self.is_layer_in_range(layer, counter)]
 
-            if not os.path.exists(os.path.join(output_path)):
-                os.makedirs(os.path.join(output_path))
+                if not os.path.exists(os.path.join(output_path)):
+                    os.makedirs(os.path.join(output_path))
 
             with tempfile.NamedTemporaryFile() as fp_svg:
                 layer_dest_svg_path = fp_svg.name
                 self.export_layers(layer_dest_svg_path, show_layer_ids)
+                filename = "%s%s" % (self.options.filename_prefix, str(counter).zfill(len(str(maxlayer))))
 
                 if self.options.filetype == "jpeg":
                     with tempfile.NamedTemporaryFile() as fp_png:
                         self.exportToPng(layer_dest_svg_path, fp_png.name)
-                        layer_dest_jpg_path = os.path.join(output_path, "%s_%s.jpg" % (str(counter).zfill(3), layer_label))
+                        layer_dest_jpg_path = os.path.join(output_path, filename + ".jpg")
                         self.convertPngToJpg(fp_png.name, layer_dest_jpg_path)
-                else:
-                    layer_dest_png_path = os.path.join(output_path, "%s_%s.png" % (str(counter).zfill(3), layer_label))
+                elif self.options.filetype == "png":
+                    layer_dest_png_path = os.path.join(output_path, filename + ".png")
                     self.exportToPng(layer_dest_svg_path, layer_dest_png_path)
-
-            counter += 1
+                elif self.options.filetype == "pdf":
+                    layer_dest_pdf_path = os.path.join(output_path, filename + ".pdf")
+                    self.exportToPdf(layer_dest_svg_path, layer_dest_pdf_path)
+                elif self.options.filetype == "latex":
+                    layer_dest_latex_path = os.path.join(output_path, filename + ".pdf")
+                    self.exportToLatex(layer_dest_svg_path, layer_dest_latex_path)
 
     def export_layers(self, dest, show):
         """
@@ -68,6 +81,8 @@ class PNGExport(inkex.Effect):
     def get_layers(self, src):
         svg_layers = self.document.xpath('//svg:g[@inkscape:groupmode="layer"]', namespaces=inkex.NSS)
         layers = []
+        expr = re.compile('\[(\d*)-(\d*)\]\s*')
+        maxlayer = 1
 
         for layer in svg_layers:
             label_attrib_name = "{%s}label" % layer.nsmap['inkscape']
@@ -77,7 +92,19 @@ class PNGExport(inkex.Effect):
             layer_id = layer.attrib["id"]
             layer_label = layer.attrib[label_attrib_name]
 
-            if layer_label.lower().startswith("[fixed] "):
+            m = expr.match(layer_label)
+            if m != None:
+                begin = 1
+                end = len(svg_layers)
+                if m.group(1):
+                    begin = int(m.group(1))
+                    maxlayer = begin if begin > maxlayer else maxlayer
+                if m.group(2):
+                    end = int(m.group(2))
+                    maxlayer = end if end > maxlayer else maxlayer
+                layer_type = ("numbered", begin, end)
+                layer_label = layer_label[len(m.group(0)):]
+            elif layer_label.lower().startswith("[fixed] "):
                 layer_type = "fixed"
                 layer_label = layer_label[8:]
             elif layer_label.lower().startswith("[export] "):
@@ -88,11 +115,25 @@ class PNGExport(inkex.Effect):
 
             layers.append([layer_id, layer_label, layer_type])
 
-        return layers
+        return (layers, maxlayer)
 
     def exportToPng(self, svg_path, output_path):
         area_param = '-D' if self.options.crop else 'C'
         command = "inkscape %s -d %s -e \"%s\" \"%s\"" % (area_param, self.options.dpi, output_path, svg_path)
+
+        p = subprocess.Popen(command.encode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p.wait()
+
+    def exportToPdf(self, svg_path, output_path):
+        area_param = '-D' if self.options.crop else 'C'
+        command = "inkscape %s -d %s --export-pdf \"%s\" \"%s\"" % (area_param, self.options.dpi, output_path, svg_path)
+
+        p = subprocess.Popen(command.encode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p.wait()
+
+    def exportToLatex(self, svg_path, output_path):
+        area_param = '-D' if self.options.crop else 'C'
+        command = "inkscape %s -d %s --export-pdf \"%s\" --export-latex \"%s\"" % (area_param, self.options.dpi, output_path, svg_path)
 
         p = subprocess.Popen(command.encode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         p.wait()
