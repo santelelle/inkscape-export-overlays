95fba8a97aae22ed9987037289211aca20b7d24c
Hendrik Fichtenberger <oss@hendrik-fichtenberger.de>
2021-06-06 18:09:27 +0200

    completed migration to Inkscape 1.0, added new feature 'multiple frame intervals'

--- export_layers.py
+++ export_layers.py
@@ -18,12 +18,11 @@ class PNGExport(inkex.Effect):
         self.arg_parser.add_argument("--path", action="store", dest="path", default="~/", help="")
         self.arg_parser.add_argument("--filename_prefix", action="store", dest="filename_prefix", default="", help="")
         self.arg_parser.add_argument('-f', '--filetype', action='store', dest='filetype', default='jpeg', help='Exported file type')
-        self.arg_parser.add_argument("--crop", action="store", type=bool, dest="crop", default=False)
         self.arg_parser.add_argument("--dpi", action="store", type=int, dest="dpi", default=90)
 
     def is_layer_in_range(self, layer, current):
-        if isinstance(layer[2], tuple) and layer[2][0] == 'numbered':
-            return layer[2][1] <= current and current <= layer[2][2]
+        if "show_on" in layer:
+            return current in layer["show_on"]
         else:
             return false
 
@@ -33,26 +32,15 @@ class PNGExport(inkex.Effect):
         layers, maxlayer = self.get_layers(curfile)
 
         for counter in range(1, maxlayer+1):
-            for (layer_id, layer_label, layer_type) in layers:
-                if layer_type == "fixed":
-                    continue
-
-                show_layer_ids = [layer[0] for layer in layers if layer[2] == "fixed" or (layer[2] == "export" and layer[0] == layer_id) or self.is_layer_in_range(layer, counter)]
-
-                if not os.path.exists(os.path.join(output_path)):
-                    os.makedirs(os.path.join(output_path))
-
+            if not os.path.exists(os.path.join(output_path)):
+                os.makedirs(os.path.join(output_path))
+            show_layer_ids = [layer["id"] for layer in layers if self.is_layer_in_range(layer, counter)]
             with tempfile.NamedTemporaryFile() as fp_svg:
                 layer_dest_svg_path = fp_svg.name
                 self.export_layers(layer_dest_svg_path, show_layer_ids)
                 filename = "%s%s" % (self.options.filename_prefix, str(counter).zfill(len(str(maxlayer))))
 
-                if self.options.filetype == "jpeg":
-                    with tempfile.NamedTemporaryFile() as fp_png:
-                        self.exportToPng(layer_dest_svg_path, fp_png.name)
-                        layer_dest_jpg_path = os.path.join(output_path, filename + ".jpg")
-                        self.convertPngToJpg(fp_png.name, layer_dest_jpg_path)
-                elif self.options.filetype == "png":
+                if self.options.filetype == "png":
                     layer_dest_png_path = os.path.join(output_path, filename + ".png")
                     self.exportToPng(layer_dest_svg_path, layer_dest_png_path)
                 elif self.options.filetype == "pdf":
@@ -81,72 +69,78 @@ class PNGExport(inkex.Effect):
     def get_layers(self, src):
         svg_layers = self.document.xpath('//svg:g[@inkscape:groupmode="layer"]', namespaces=inkex.NSS)
         layers = []
-        expr = re.compile('\[(\d*)-(\d*)\]\s*')
+        expr = re.compile('\[(\d*-\d*(,\d*-\d*)*)\]')
         maxlayer = 1
 
-        for layer in svg_layers:
-            label_attrib_name = "{%s}label" % layer.nsmap['inkscape']
-            if label_attrib_name not in layer.attrib:
+        for svg_layer in svg_layers:
+            svg_label_attrib_name = "{%s}label" % svg_layer.nsmap['inkscape']
+            if svg_label_attrib_name not in svg_layer.attrib:
                 continue
+            svg_layer_id = svg_layer.attrib["id"]
+            svg_layer_label = svg_layer.attrib[svg_label_attrib_name]
 
-            layer_id = layer.attrib["id"]
-            layer_label = layer.attrib[label_attrib_name]
-
-            m = expr.match(layer_label)
+            layer_prop = { "id" : svg_layer_id }
+            m = expr.match(svg_layer_label)
             if m != None:
-                begin = 1
-                end = len(svg_layers)
-                if m.group(1):
-                    begin = int(m.group(1))
-                    maxlayer = begin if begin > maxlayer else maxlayer
-                if m.group(2):
-                    end = int(m.group(2))
-                    maxlayer = end if end > maxlayer else maxlayer
-                layer_type = ("numbered", begin, end)
-                layer_label = layer_label[len(m.group(0)):]
-            elif layer_label.lower().startswith("[fixed] "):
-                layer_type = "fixed"
-                layer_label = layer_label[8:]
-            elif layer_label.lower().startswith("[export] "):
-                layer_type = "export"
-                layer_label = layer_label[9:]
+                layer_visible = []
+                all_frames = m.group(1).split(",")
+                for frames in all_frames:
+                    split_boundaries = frames.split("-")
+                    if len(split_boundaries) != 2:
+                        continue
+                    boundaries = {}
+                    if split_boundaries[0]:
+                        boundaries["begin"] = int(split_boundaries[0])
+                        maxlayer = max(maxlayer, boundaries["begin"])
+                    if split_boundaries[1]:
+                        boundaries["end"] = int(split_boundaries[1])
+                        maxlayer = max(maxlayer, boundaries["end"])
+                    layer_visible += [boundaries]
+                layer_prop["show_intervals"] = layer_visible
+                layer_prop["type"] = "numbered"
+            elif svg_layer_label.lower().startswith("[fixed]"):
+                layer_prop["type"] = "fixed"
             else:
                 continue
-
-            layers.append([layer_id, layer_label, layer_type])
+            layers.append(layer_prop)
+        
+        for layer in layers:
+            layer["show_on"] = []
+            if layer["type"] == "fixed":
+                layer["show_on"] += [*range(1, maxlayer+1)]
+            elif layer["type"] == "numbered":
+                layer["show_on"] = []
+                for interval in layer["show_intervals"]:
+                    begin = interval["begin"] if "begin" in interval else 1
+                    end = interval["end"] if "end" in interval else maxlayer
+                    layer["show_on"] += [*range(begin, end+1)]
+            inkex.debug(layer)
 
         return (layers, maxlayer)
 
     def exportToPng(self, svg_path, output_path):
-        area_param = '-C' #'-D' if self.options.crop else '-C'
-        command = "inkscape %s -d %s -e \"%s\" \"%s\"" % (area_param, self.options.dpi, output_path, svg_path)
+        area_param = '-C' 
+        command = "inkscape %s -d %s -o \"%s\" \"%s\"" % (area_param, self.options.dpi, output_path, svg_path)
 
         with subprocess.Popen(command.encode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
             p.wait()
             p.kill()
 
     def exportToPdf(self, svg_path, output_path):
-        area_param = '-C' #'-D' if self.options.crop else '-C'
-        command = "inkscape %s -d %s --export-pdf \"%s\" \"%s\"" % (area_param, self.options.dpi, output_path, svg_path)
+        area_param = '-C'
+        command = "inkscape %s -d %s -o \"%s\" \"%s\"" % (area_param, self.options.dpi, output_path, svg_path)
 
         with subprocess.Popen(command.encode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
             p.wait()
             p.kill()
 
     def exportToLatex(self, svg_path, output_path):
-        area_param = '-C' #'-D' if self.options.crop else '-C'
-        command = "inkscape %s -d %s --export-filename \"%s\" --export-latex \"%s\"" % (area_param, self.options.dpi, output_path, svg_path)
+        area_param = '-C'
+        command = "inkscape %s -d %s -o \"%s\" --export-latex \"%s\"" % (area_param, self.options.dpi, output_path, svg_path)
 
         with subprocess.Popen(command.encode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
             p.wait()
             p.kill()
-
-    def convertPngToJpg(self, png_path, output_path):
-        command = "convert \"%s\" \"%s\"" % (png_path, output_path)
-        with subprocess.Popen(command.encode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
-            p.wait()
-            p.kill()
-
 
 def _main():
     e = PNGExport()
